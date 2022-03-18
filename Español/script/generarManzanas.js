ba016fd79c2798fe6cfd83f705180f1417c92f7b
kinire98 <iknitgarri@outlook.com>
2022-03-18 09:10:22 +0100

    Se cambió el nombre de algunos archivos a inglés y se añadieron comentrarios

--- Español/script/generarManzanas.js
+++ Español/script/generarManzanas.js
@@ -1,20 +1,18 @@
-function generarNumeroAleatorio (min,max) {
-    return Math.round(Math.random()*((max-min)+min))
+function generarNumeroAleatorio (min,max) { //Devuelve un número aleatoria, así la posición de la manzana no sigue ningún patrón
+    return Math.round(Math.random()*(max-min)+min)
 }
 
 
-function ponerMazana (posicion) {
-    let x = generarNumeroAleatorio(0,ancho)
+function ponerMazana (posicion) { //Esta función genera una nueva posición para la manzana. Se ejecuta cuando la cabeza de la serpiente pasa por encima de la manzana.
+    //También se ejecuta solo al principio del juego
+    let x = generarNumeroAleatorio(0,ancho) // proporciona valores aleatorios para las coordenadas de la manzana, entre 0 y el ancho o el alto del área de juego
     let y = generarNumeroAleatorio(0,alto)
-    if ((x == posicion[0].ancho)&&(y == posicion[0].alto)) {
-        return ponerMazana (posicion);
-         
-    } else {
-            for (let i = 0; i < posicion.length - 1; i++) {
-                if ((x == posicion[i].ancho) || (y == posicion[i].alto)) {
-                    return ponerMazana (posicion);
-                }
-            }
-            return [x,y]
+    for (let i = 0; i < posicion.length - 1; i++) { // Este bucle comprueba que la manzana no aparezca en dónde está en ese momento la serpiente
+        //si la posicion de la manzana es  igual a una de la serpiente, se devuelve la ejecución de la misma función de nuevo para que se pueda volver a hallar la posición de la manzana
+        if ((x == posicion[i].ancho) && (y == posicion[i].alto)) {
+               return ponerMazana (posicion);
+        }
     }
+    return [x,y] //Si no es igual se devuelven las coordenadas de las manzana
+
 }
